69e4362caed1159a7f9d3c8a67388e2a5344099e
gabrielmartinigit <gabrielb.martini@gmail.com>
2021-05-21 15:00:22 -0300

    feat(Question) Loading image

--- src/content/QuestionDetails.jsx
+++ src/content/QuestionDetails.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Grid from 'aws-northstar/layouts/Grid';
-import Placeholder from 'aws-northstar/components/Placeholder';
 import Container from 'aws-northstar/layouts/Container';
 import api from '../services/api';
 import { embedDashboard } from 'amazon-quicksight-embedding-sdk';
+import { useLocation } from "react-router-dom";
 
 function QuestionDetails() {
+    /* START GET DASHBOARD 
     const dashboardRef = React.createRef();
 
     const embed = async () => {
@@ -28,16 +29,39 @@ function QuestionDetails() {
 
     React.useEffect(() => {
         embed();
-    });
+    }, []);
+    /* END GET DASHBOARD */
+
+    /* START GET IMAGE */
+    const dataQuestion = useLocation();
+    const [imageUrl, setImageUrl] = React.useState();
+
+    React.useEffect(() => {
+        api.get('question_url', {
+            params: {
+                object_key: dataQuestion.state.object_key,
+                bucket: dataQuestion.state.bucket
+            }
+        }).then((response) => {
+            setImageUrl(response.data['presignedUrl'])
+        });
+    }, []);
+    /* END GET IMAGE */
 
     return (
         <Container headingVariant='h1' title="Detalhamento da Questão">
             <Grid container spacing={3}>
                 <Grid item xs={6}>
-                    <div ref={dashboardRef} />
+                    {/* <div ref={dashboardRef} /> */}
                 </Grid>
                 <Grid item xs={6}>
-                    <Placeholder />
+                    <img
+                        style={{
+                            height: 'auto',
+                            width: '100%'
+                        }}
+                        src={imageUrl}
+                    />
                 </Grid>
             </Grid>
         </Container>
--- src/content/Questions.jsx
+++ src/content/Questions.jsx
@@ -1,95 +1,111 @@
-import React from 'react'
+import React from 'react';
 import Table from 'aws-northstar/components/Table';
 import Container from 'aws-northstar/layouts/Container';
 import Inline from 'aws-northstar/layouts/Inline';
 import Alert from 'aws-northstar/components/Alert';
 import Button from 'aws-northstar/components/Button';
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 import api from '../services/api';
 import './styles/questions.css';
 
 function Questions() {
-    /* START TABLE */
-
+    /* START TABLE DEFINITION*/
     const columnDefinitions = [
         {
             id: 'book',
             width: 200,
             Header: 'Caderno',
-            accessor: 'book'
+            accessor: 'book',
         },
         {
             id: 'year',
             width: 200,
             Header: 'Ano',
-            accessor: 'year'
+            accessor: 'year',
         },
         {
             id: 'page',
             width: 200,
             Header: 'Página',
-            accessor: 'page'
+            accessor: 'page',
         },
         {
             id: 'question',
             width: 200,
             Header: 'Número',
-            accessor: 'question'
+            accessor: 'question',
         },
         {
             id: 'area',
             width: 200,
             Header: 'Área',
-            accessor: 'area'
-        }
+            accessor: 'area',
+        },
     ];
+    /* END TABLE DEFINITION*/
+
+    /* START TABLE DATA */
+    const [dataQuestions, setDataQuestions] = React.useState();
+
+    React.useEffect(() => {
+        api.get('questions').then((response) => {
+            const dataEnem = [];
+            response.data['items'].forEach((item, index) => {
+                dataEnem.push({
+                    question: item['question_num']['S'],
+                    book: item['color']['S'],
+                    year: 2019,
+                    page: item['page_number']['S'],
+                    area: item['exam_area']['S'],
+                    object_key: item['object_key']['S'],
+                    bucket: item['bucket_name']['S']
+                });
+            });
+            setDataQuestions(dataEnem);
+        });
+    }, []);
+    /* END TABLE DATA */
+
+    /* START TABLE ACTION */
+    const [dataQuestion, setDataQuestion] = React.useState();
 
     const tableActions = (
         <Inline>
             <Button variant='primary'>
-                <Link to="/questions/teste">Detalhar</Link>
+                <Link
+                    style={{
+                        textDecoration: 'none',
+                        color: 'white'
+                    }}
+                    to={
+                        dataQuestion === undefined ? '/questions' :
+                            {
+                                pathname: `/questions/${dataQuestion.question}-${dataQuestion.book}-${dataQuestion.year}`,
+                                state: dataQuestion
+                            }
+                    }>Detalhar</Link>
             </Button>
         </Inline>
     );
 
-    /* END TABLE */
-
-    /* START TABLE DATA */
-    const [data, setData] = React.useState();
-
-    React.useEffect(() => {
-        api.get('questions').then(
-            response => {
-                const dataEnem = []
-                response.data['items'].forEach((item, index) => {
-                    dataEnem.push(
-                        {
-                            question: item['question_num']['S'],
-                            book: item['color']['S'],
-                            year: 2019,
-                            page: item['page_number']['S'],
-                            area: item['exam_area']['S']
-                        }
-                    )
-                });
-                setData(dataEnem)
-            }
-        )
-    }, []);
-    /* END TABLE DATA */
+    const handleQuestionSelect = (event) => {
+        setDataQuestion(event[0]);
+    }
+    /* END TABLE ACTION */
 
     return (
         <Container headingVariant='h1' title='Questões do ENEM'>
-            <Alert type="info" dismissable={true}>
-                Para visualizar mais informações sobre a questão clique em <b>Detalhes</b>.
-            </Alert>
+            <Alert type='info' dismissable={true}>
+                Para visualizar mais informações sobre a questão clique em{' '}
+                <b>Detalhar</b>.
+      </Alert>
             <br></br>
             <Table
-                onSelectionChange={() => { }}
+                onSelectionChange={handleQuestionSelect}
                 columnDefinitions={columnDefinitions}
                 actionGroup={tableActions}
-                items={data}
+                items={dataQuestions}
                 multiSelect={false}
                 disableGroupBy={true}
                 disableSettings={false}
@@ -98,7 +114,7 @@ function Questions() {
                 disableRowSelect={false}
                 disableSortBy={false}
             />
-        </Container >
+        </Container>
     );
 }
 
